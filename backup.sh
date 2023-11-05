0000206a0ba2a20270a56589b420d371652635e6
Youcef Saidi <saidi.ppv@gmail.com>
2023-11-05 14:17:52 +0100

    fix backup folder

--- backup.sh
+++ backup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+NOW=$(date +"%Y-%m-%d_%H-%M-%S")
+
+# Filename for backup
+BACKUP_FILE="/backups/db-$NOW"
+
+
 # Start the backup and time it
 START=$(date +%s)
 PGPASSWORD="${DB_PASSWORD}" pg_dump -h "$DB_HOST" -U "$DB_USER" -Fd "$DB_NAME" -f "$BACKUP_FILE" -j 5 -v
